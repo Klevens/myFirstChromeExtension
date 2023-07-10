d673e0d8a7342f427d7a5c98b2df58cf3b394800
Klevens <kelvinreyescnh@gmail.com>
2023-07-10 18:28:52 -0400

    adding things

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# myFirstChromeExtension
+## About
+
+
+This Extension get random Pokemon from <a href="https://pokeapi.co/"> PokéAPI </a>.
+
+<br></br>
+**Getting Started** 
+
+This is my first ChromeExtension.
+
+**Few Images**
+
+<div align="center">
+  <br />
+    <p>
+        <h2>First state</h2>
+        <img src="./Captures/firstStart.png" alt="discord.js" />
+    </p>
+  <br />
+    <p>
+        <h2>After click random button</h2>
+        <img src="./Captures/afterClick.png" alt="discord.js" />
+    </p>
+</div>
+
+
+
+
--- popup.js
+++ popup.js
@@ -1,8 +1,35 @@
 
-const message = document.querySelector("div");
-const boton = document.querySelector('#change-message');
+const boton = document.querySelector('#change-pokemon');
+const contenedor = document.querySelector("#contenedor");
 
 boton.addEventListener('click', ()=> {
-    message.textContent = "nuevo mensaje";
-    alert("HolaMundo")
+    
+    let pokeId = Math.floor(Math.random() * 600) + 1; 
+    traerDatos(pokeId)
 })
+
+
+function traerDatos(id){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(response => response.json())
+    .then(function(data){
+      let nombre = data.name;
+      let url = data.sprites.other.dream_world.front_default;
+      if(nombre && url)
+      {
+          imprimirTarjeta(nombre,url);
+      }
+    });
+  }
+  
+  function imprimirTarjeta(nombre,url)
+    
+  {
+    let template = `<img class="pokemon" src="${url}" alt="" >
+      <br>
+      <label for="">
+        ${nombre}
+      </label> <br>
+      `;
+    contenedor.innerHTML = template;
+  }
